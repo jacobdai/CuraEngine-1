6bf1cdd723a7a82bdc94ded1f5dab2c157bd4723
jacobdai <503564236@qq.com>
2015-08-14 15:27:11 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -610,8 +610,9 @@ void GCodePlanner::writeGCode(bool liftHeadIfNeeded, int layerThickness)
         GCodePath* path = &paths[n];
         while(1)
          {
-             unsigned int temp=path->points.size;
-            if(1)
+            unsigned int temp=path->points.size();
+            optimizePolygons（path->points）；
+            if(temp==path->points.size())
             break;
         }
         if (extruder != path->extruder)
