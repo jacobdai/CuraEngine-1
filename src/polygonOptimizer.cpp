d1cdee51713a5d00b9f1eea3d5928ff1917b582e
jacobdai <503564236@qq.com>
2015-08-17 20:08:26 +0800

    Update polygonOptimizer.cpp

--- src/polygonOptimizer.cpp
+++ src/polygonOptimizer.cpp
@@ -58,44 +58,22 @@ void optimizePolygons(Polygons& polys)
     }
 }
 void optimizePolygonadd(GCodePath* path)
-{ 
-  Point p0 = path->points[path->points.size()-1];
-  Point p1 = path->points[0];
-  if (shorterThen(p0 - p1, MICRON2INT(10)))
-  {
-    path->points[0]=path->points[path->points.size()-1];
-    path->points.erase(path->points.begin() );
-  }else if (shorterThen(p0 - p1, MICRON2INT(500)))
-  {
-    Point p2;
-            if ( path->points.size() > 1)
-                p2 = path->points[1];
-            else
-                p2 = path->points[0];
-          
-            Point diff0 = normal(p1 - p0, 10000000);
-            Point diff2 = normal(p1 - p2, 10000000);
-            
-            int64_t d = dot(diff0, diff2);
-            if (d < -99999999999999LL)
-            {
-                path->points[0]=path->points[path->points.size()-1];
-            }
-  }
-p0 = path->points[0];
-  for(unsigned int i=1;i<path->points.size();i++)
+{
+    Point p0 = path->points[path->points.size()-1];
+    for(unsigned int i=0;i<path->points.size();i++)
     {
         Point p1 = path->points[i];
         if (shorterThen(p0 - p1, MICRON2INT(10)))
         {
-            path->points[i]=path->points[i-1];
+            path->points.erase(path->points.begin()+i);
+            i --;
         }else if (shorterThen(p0 - p1, MICRON2INT(500)))
         {
             Point p2;
-            if (i < path->points.size() - 1)
-                p2 = path->points[i+1];
+            if (i < poly.size() - 1)
+                p2 = poly[i+1];
             else
-                p2 = path->points[0];
+                p2 = poly[0];
             
             Point diff0 = normal(p1 - p0, 10000000);
             Point diff2 = normal(p1 - p2, 10000000);
@@ -103,7 +81,8 @@ p0 = path->points[0];
             int64_t d = dot(diff0, diff2);
             if (d < -99999999999999LL)
             {
-                path->points[i]=path->points[i-1];
+                path->points.erase(path->points.begin()+i);
+                i --;
             }else{
                 p0 = p1;
             }
@@ -112,7 +91,6 @@ p0 = path->points[0];
         }
     }
 }
-
 
 
 
