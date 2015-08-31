ff381f8704117c1871c550483b9baf0f16b79174
jacobdai <503564236@qq.com>
2015-08-31 19:40:07 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -253,6 +253,9 @@ void GCodeExport::writeMove(Point p, int speed, int lineWidth)
             }
         }
         fprintf(f, "G1 X%0.3f Y%0.3f Z%0.3f F%0.1f\r\n", INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y), INT2MM(zPos), fspeed);
+        currentPosition = Point3(p.X, p.Y, zPos);
+        startPosition = currentPosition;
+        estimateCalculator.plan(TimeEstimateCalculator::Position(INT2MM(currentPosition.x), INT2MM(currentPosition.y), INT2MM(currentPosition.z), extrusionAmount), speed);
     }else{
         
         //Normal E handling.
@@ -287,36 +290,41 @@ void GCodeExport::writeMove(Point p, int speed, int lineWidth)
             fprintf(f, " F%i", speed * 60);
             currentSpeed = speed;
         }
-                float xpos=currentPosition.x;
-		float ypos=currentPosition.y;
-		float xnext=p.X - extruderOffset[extruderNr].X;
-		float ynext=p.Y - extruderOffset[extruderNr].Y;
-		if((((xpos-xnext)>100000.0)||((xpos-xnext)<-100000.0)||((ypos-ynext)>100000.0)||((ypos-ynext)<-100000.0))&&(lineWidth == 0))
-		{
-			fprintf(f, " X%0.3f Y%0.3f", INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y));
-            if (zPos != currentPosition.z)
-		    {
+        float xpos=currentPosition.x;
+	float ypos=currentPosition.y;
+	float xnext=p.X - extruderOffset[extruderNr].X;
+	float ynext=p.Y - extruderOffset[extruderNr].Y;
+	if((((xpos-xnext)>100000.0)||((xpos-xnext)<-100000.0)||((ypos-ynext)>100000.0)||((ypos-ynext)<-100000.0))&&(lineWidth == 0))
+	{
+	  fprintf(f, " X%0.3f Y%0.3f", INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y));
+          if (zPos != currentPosition.z)
+	  {
                fprintf(f, " Z%0.3f", INT2MM(zPos));
-			}else
-			{
-				float zadd=currentPosition.z+10000;
-               fprintf(f, " Z%0.3f", INT2MM(zadd));
-			}
-            fprintf(f, "\n");
-		}else
-		{
-			fprintf(f, " X%0.3f Y%0.3f", INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y));
-            if (zPos != currentPosition.z)
-                 fprintf(f, " Z%0.3f", INT2MM(zPos));
-            if (lineWidth != 0)
-                 fprintf(f, " %c%0.5f", extruderCharacter[extruderNr], extrusionAmount);
-            fprintf(f, "\n");
-		}
+               currentPosition = Point3(p.X, p.Y, zPos);
+               startPosition = currentPosition;
+               estimateCalculator.plan(TimeEstimateCalculator::Position(INT2MM(currentPosition.x), INT2MM(currentPosition.y), INT2MM(currentPosition.z), extrusionAmount), speed);
+	  }else
+	  {
+		loat zadd=currentPosition.z+10000;
+                fprintf(f, " Z%0.3f", INT2MM(zadd));
+                currentPosition = Point3(p.X, p.Y, zadd);
+                startPosition = currentPosition;
+                estimateCalculator.plan(TimeEstimateCalculator::Position(INT2MM(currentPosition.x), INT2MM(currentPosition.y), INT2MM(currentPosition.z), extrusionAmount), speed);
+	  }
+          fprintf(f, "\n");
+	}else
+	  {
+		fprintf(f, " X%0.3f Y%0.3f", INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y));
+                if (zPos != currentPosition.z)
+                fprintf(f, " Z%0.3f", INT2MM(zPos));
+                if (lineWidth != 0)
+                fprintf(f, " %c%0.5f", extruderCharacter[extruderNr], extrusionAmount);
+                fprintf(f, "\n");
+                currentPosition = Point3(p.X, p.Y, zPos);
+                startPosition = currentPosition;
+                estimateCalculator.plan(TimeEstimateCalculator::Position(INT2MM(currentPosition.x), INT2MM(currentPosition.y), INT2MM(currentPosition.z), extrusionAmount), speed);
+	  }
     }
-    
-    currentPosition = Point3(p.X, p.Y, zPos);
-    startPosition = currentPosition;
-    estimateCalculator.plan(TimeEstimateCalculator::Position(INT2MM(currentPosition.x), INT2MM(currentPosition.y), INT2MM(currentPosition.z), extrusionAmount), speed);
 }
 
 void GCodeExport::writeRetraction(bool force)
