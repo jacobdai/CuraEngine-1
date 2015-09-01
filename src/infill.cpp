7f946d202bad1fcec9ffbb1fd4d6b614af0578f4
jacobdai <503564236@qq.com>
2015-09-01 13:23:46 +0800

    Update infill.cpp

--- src/infill.cpp
+++ src/infill.cpp
@@ -55,11 +55,11 @@ void generateLineInfill(const Polygons& in_outline, Polygons& result, int extrus
     Polygons outline = in_outline.offset(extrusionWidth * infillOverlap / 100);
     AABB boundaryin(outline);
 	double rotationin=rotation;
-	if ((boundaryin.max.X-boundaryin.min.X)<150)
+	if ((boundaryin.max.X-boundaryin.min.X)<150000)
 		{
 			rotationin=30;
 	     }
-	else if((boundaryin.max.Y-boundaryin.min.Y)<150)
+	else if((boundaryin.max.Y-boundaryin.min.Y)<150000)
 	    {
             rotationin=60;
 	     }
