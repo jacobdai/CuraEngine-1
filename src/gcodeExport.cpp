b489787e392543de7321e2903d7b97fbf60a87a2
jacobdai <503564236@qq.com>
2015-09-07 20:22:42 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -212,7 +212,7 @@ void GCodeExport::writeDelay(double timeAmount)
     fprintf(f, "G4 P%d\n", int(timeAmount * 1000));
     totalPrintTime += timeAmount;
 }
-void GCodeExport::writeArc(Point p, int speed, int lineWidth,float ra,int clk,Point pcenter)
+void GCodeExport::writeArc(Point p, int speed, int lineWidth,float ra,int coli,int colj,int clk,Point pcenter)
 {
     Point pcur = getPositionXY();
     Point diff = p - getPositionXY();
@@ -228,21 +228,24 @@ void GCodeExport::writeArc(Point p, int speed, int lineWidth,float ra,int clk,Po
                     resetExtrusionValue();
                 isRetracted = false;
             }
-    long long int clockarc=(INT2MM(pcenter.X)-INT2MM(pcur.X))*(INT2MM(p.Y)-INT2MM(pcenter.Y))-(INT2MM(pcenter.Y)-INT2MM(pcur.Y))*(INT2MM(p.X)-INT2MM(pcenter.X));
+    Point pcuro;
+    pcuro.X=2*pcenter.X-pcur.X;
+    pcuro.Y=2*pcenter.Y-pcur.Y;
+    long long int clockarc=(INT2MM(p.X)-INT2MM(pcur.X))*(INT2MM(pcuro.Y)-INT2MM(p.Y))-(INT2MM(p.Y)-INT2MM(pcur.Y))*(INT2MM(pcuro.X)-INT2MM(p.X));
     double as=vSizeMM(diff)/(2*ra);
     if(as>1)
 	as=1;
     double Arc=2*ra*asin(as);
-    if(((clockarc<0)&&(clk<0))||((clockarc>0)&&(clk>0)))
+    if(((clockarc>0)&&(clk<0))||((clockarc<0)&&(clk>0)))
     {
 		Arc=2*M_PI*ra-Arc;
 		ra=(-1)*ra;
     }
     extrusionAmount += extrusionPerMM * INT2MM(lineWidth) * Arc;
 	if(clk>0)
-	    fprintf(f, "G03 F%i X%0.3f Y%0.3f R%0.3f %c%0.5f\n",speed * 60,INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y),ra, extruderCharacter[extruderNr], extrusionAmount);
+	    fprintf(f, "G03 F%i X%0.3f Y%0.3f I%i J%i %c%0.5f\n",speed * 60,INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y),INT2MM(coli), INT2MM(colj), extruderCharacter[extruderNr], extrusionAmount);
 	if(clk<0)
-	    fprintf(f, "G02 F%i X%0.3f Y%0.3f R%0.3f %c%0.5f\n",speed * 60,INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y),ra, extruderCharacter[extruderNr], extrusionAmount);
+	    fprintf(f, "G02 F%i X%0.3f Y%0.3f I%i J%i %c%0.5f\n",speed * 60,INT2MM(p.X - extruderOffset[extruderNr].X), INT2MM(p.Y - extruderOffset[extruderNr].Y),INT2MM(coli), INT2MM(colj), extruderCharacter[extruderNr], extrusionAmount);
 
     currentPosition = Point3(p.X, p.Y, zPos);
     startPosition = currentPosition;
@@ -802,7 +805,9 @@ void GCodePlanner::writeGCode(bool liftHeadIfNeeded, int layerThickness)
 					if((l>(i+oN+3))&&(r1<500))
 						{
 							int clock=(INT2MM(VolumeP2.X)-INT2MM(VolumeP1.X))*(INT2MM(VolumeP3.Y)-INT2MM(VolumeP2.Y))-(INT2MM(VolumeP2.Y)-INT2MM(VolumeP1.Y))*(INT2MM(VolumeP3.X)-INT2MM(VolumeP2.X));
-							gcode.writeArc(path->points[l-1],speed,path->config->lineWidth,r1,clock,VolumeO1);
+							int coi=VolumeP1.X-VolumeO1.X;
+							int coj=VolumeP1.Y-VolumeO1.Y;
+							gcode.writeArc(path->points[l-1],speed,path->config->lineWidth,r1,coi,coj,clock,VolumeO1);
 							i=l;
 						}
 
