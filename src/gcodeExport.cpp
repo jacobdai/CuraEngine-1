318d215216015d033f2c2f75083e8842323fe1b4
jacobdai <503564236@qq.com>
2015-09-07 15:51:41 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -230,7 +230,7 @@ void GCodeExport::writeArc(Point p, int speed, int lineWidth,int r,int clk,Point
                 isRetracted = false;
             }
     long long int clockarc=(INT2MM(pcenter.X)-INT2MM(pcur.X))*(INT2MM(p.Y)-INT2MM(pcenter.Y))-(INT2MM(pcenter.Y)-INT2MM(pcur.Y))*(INT2MM(p.X)-INT2MM(pcenter.X));
-    double as=vSizeMM(diff))/(2*r);
+    double as=vSizeMM(diff)/(2*r);
     if(as>1)
 	as=1;
     double Arc=2*r*asin(as);
