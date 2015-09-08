8a2ead09b2ca0534e3f63a704dd54e0077d1a572
jacobdai <503564236@qq.com>
2015-09-08 15:23:45 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -784,7 +784,7 @@ void GCodePlanner::writeGCode(bool liftHeadIfNeeded, int layerThickness)
 					   {
 							   lx0=INT2MM(path->points[l].X);
 							   ly0=INT2MM(path->points[l].Y);
-							   ld=sqrt(lA*lx0+ly0+lC)/sqrt(lA*lA+1);
+							   ld=sqrt(fabs(lA*lx0+ly0+lC))/sqrt(lA*lA+1);
 							   if(ld>1)
 								   break;
 					    }
