19933e53cc87d3319f10a49211738bb8493bfe1c
jacobdai <503564236@qq.com>
2015-09-28 20:11:13 +0800

    Update fffProcessor.h

--- src/fffProcessor.h
+++ src/fffProcessor.h
@@ -639,7 +639,7 @@ private:
                 if (layerNr > 0)
                     bridge = bridgeAngle(outline, &storage.volumes[volumeIdx].layers[layerNr-1]);
                 int lsp=1.2*extrusionWidth;
-                    generateLineInfill(outline, skinPolygons, extrusionWidth, lsp, config.infillOverlap, (bridge > -1) ? bridge : fillAngle);
+                    generateLineInfill(outline, skinPolygons, extrusionWidth, lsp,fillAngle);
             }
             if (config.enableCombing == COMBING_NOSKIN)
             {
