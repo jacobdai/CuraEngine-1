ac8fd2172418406b9b7450df84b854a422291e3e
jacobdai <503564236@qq.com>
2015-10-12 16:01:39 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -319,10 +319,10 @@ void GCodeExport::writeMove(Point p, int speed, int lineWidth)
             }
             if(vSizeMM(diff)<200)
             {
-                extrusionAmount += ((extrusionPerMM * INT2MM(lineWidth) * vSizeMM(diff)*0.8)/1.2);
-            }else if(vSizeMM(diff)<400)
+                extrusionAmount += ((extrusionPerMM * INT2MM(lineWidth) * vSizeMM(diff)*0.9)/1.2);
+            }else if(vSizeMM(diff)<300)
             {
-            	extrusionAmount += ((extrusionPerMM * INT2MM(lineWidth) * vSizeMM(diff)*0.9)/1.2);
+            	extrusionAmount += ((extrusionPerMM * INT2MM(lineWidth) * vSizeMM(diff)*0.95)/1.2);
             }else
             {
             	extrusionAmount += ((extrusionPerMM * INT2MM(lineWidth) * vSizeMM(diff))/1.2);
