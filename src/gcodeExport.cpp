a62c800034f56c2ae3d68819c13f97b993cf5cd2
jacobdai <503564236@qq.com>
2015-12-22 15:42:54 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -777,7 +777,9 @@ void GCodePlanner::writeGCode(bool liftHeadIfNeeded, int layerThickness)
         	
             for(unsigned int i=0; i<path->points.size(); i++)
             {
-            	if(path->config->name=="FILL")
+            	char* name_1 ="SKIN";
+            	char* name_2 =path->config->name;
+            	if(strcmp(name_1,name_1)==0)
             	{
             	   extrusionisskin=true;
             	}
