c6cff67f0735ed55c725f57df16910ceac246a1a
jacobdai <503564236@qq.com>
2015-12-22 15:53:52 +0800

    Update gcodeExport.cpp

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -777,7 +777,7 @@ void GCodePlanner::writeGCode(bool liftHeadIfNeeded, int layerThickness)
         	
             for(unsigned int i=0; i<path->points.size(); i++)
             {
-            	const char* name_1 ="SKIN";
+            	const char* name_1 ="FILL";
             	const char* name_2 =path->config->name;
             	if(strcmp(name_1,name_1)==0)
             	{
