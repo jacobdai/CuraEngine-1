f9259dbd3898867407b567404e5292365d46e49e
jacobdai <503564236@qq.com>
2015-12-25 15:48:36 +0800

    Update fffProcessor.h

--- src/fffProcessor.h
+++ src/fffProcessor.h
@@ -479,7 +479,7 @@ private:
 
             gcode.writeComment("LAYER:%d", layerNr);
             if (((layerNr%3)==0)&&(layerNr!=0))
-                gcode.setExtrusion(config.initialLayerThickness, config.filamentDiameter, Flowadd8);
+                gcode.setExtrusion(config.initialLayerThickness, config.filamentDiameter, Flowadd10);
             else
                 gcode.setExtrusion(config.initialLayerThickness, config.filamentDiameter, Flowadd038);
             /*else if(layerNr==1)
